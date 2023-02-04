8e9de5d87038afdaf29fabfd950f4dc155945624
chazbz42cha <cha25.bz42@gmail.com>
2023-02-04 10:44:18 +0100

    up

---
+++ index.js
@@ -0,0 +1,14 @@
+const checkbox = document.querySelector("#flexSwitchCheckDefault");
+const navbar = document.querySelector("nav")
+const body = document.querySelector("body");
+
+const titres = document.querySelectorAll("h5");
+
+checkbox.addEventListener('click', function() {
+    body.classList.toggle("darkmode");
+    titres.forEach(titre => {
+        titre.classList.toggle("darkmode");
+    })
+
+
+});
